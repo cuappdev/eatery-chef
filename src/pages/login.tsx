97be3ff8bb273dda6214d9556e139b918e36f7f1
Justin Ngai <jn537@cornell.edu>
2022-11-14 00:13:01 -0500

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -64,7 +64,7 @@ export default function loginPage() {
             </Typography>
 
             <Grid container spacing={4}>
-              {dummyData.map((eatery: Eatery) => (
+              {eateries.map((eatery: Eatery) => (
                 <Grid item xs={6}>
                   <EateryCard
                     key={eatery.imgName}
