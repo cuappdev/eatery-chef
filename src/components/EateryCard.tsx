9fc5bfbcb4cd481f92464547716ff3f06253881d
Justin Ngai <jn537@cornell.edu>
2022-11-15 19:51:26 -0500

    clean up imports

--- src/components/EateryCard.tsx
+++ src/components/EateryCard.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
-// import { StaticImage } from 'gatsby-plugin-image'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Typography from '@material-ui/core/Typography'
+import { Card, CardActionArea, CardMedia, Paper } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
 import styled from '@emotion/styled'
 
 const useStyles = makeStyles((theme) => ({
